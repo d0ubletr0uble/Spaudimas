53aa8f57ba1ff6fd873d94167acefd9b82cb590b
d0ubletr0uble <Tomas70333481@gmail.com>
2019-10-06 15:17:17 +0300

    TODO listView

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String FILE = "Spaudimas.csv";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.history)
+            startActivity(new Intent(this, HistoryActivity.class));
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void handleClick(View v) {
+        // Get all data needed
+        String upper = ((EditText) findViewById(R.id.pressureHigh)).getText().toString();
+        String lower = ((EditText) findViewById(R.id.pressureLow)).getText().toString();
+        String pulse = ((EditText) findViewById(R.id.heartbeat)).getText().toString();
+        String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        String time = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
+
+        getStoragePermissions();
+
+        writeToFile(String.format("%s;%s;%s|%s|%s%s", date, time, upper, lower, pulse, System.lineSeparator()));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void writeToFile(String message) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), FILE);
+
+        //Check if file has headers
+        boolean headerFlag = false;
+        if (!file.exists())
+            headerFlag = true;
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file, true);
+            if (headerFlag)
+                fos.write("sep=;\nData;Laikas;Virsutinis|Apatinis|Pulsas\n".getBytes());
+
+            fos.write(message.getBytes());
+            fos.close();
+            playSound();
+
+            Toast.makeText(this, "Išsaugota", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "KLAIDA", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private void getStoragePermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "Prieiga suteikta, paspauskite mygtuką dar kartą", Toast.LENGTH_SHORT).show();
+    }
+
+    private void playSound() {
+        MediaPlayer player = MediaPlayer.create(this, R.raw.pop);
+        player.start();
+    }
+
+}
